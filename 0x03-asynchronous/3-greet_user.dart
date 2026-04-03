8efa11a90d5c1398be0f06e82062442bc1517527
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 13:50:02 +0200

    Task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,22 @@
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    final user = await fetchUserData();
+    return 'Hello $user';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    final credentials = await checkCredentials();
+    if (credentials) {
+      return await greetUser();
+    }
+    return 'Wrong credentials';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
