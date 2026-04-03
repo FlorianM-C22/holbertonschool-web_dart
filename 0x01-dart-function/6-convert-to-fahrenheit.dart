c93c464b04bdf3fe1dc3f4b7fcaebdc5a527019f
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 11:38:28 +0200

    Fix task 5

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,3 +1,3 @@
 List<double> convertToF(List<double> temperaturesInC){
-    return temperaturesInC.map((c) => (c * 9 / 5 + 32)).toList();
+    return temperaturesInC.map(((c) => (c * 9 / 5 + 32)).toList().map((e) => double.parse(e.toStringAsFixed(2))).toList();
 }
