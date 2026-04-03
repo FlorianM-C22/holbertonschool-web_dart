5c536fce4989974dc3be6affd9999bc39a7d05e9
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 13:21:49 +0200

    Task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,16 @@
+class Password {
+  String _password = "";
+
+  bool isValid() {
+    return _password.length >= 8 &&
+        _password.length <= 16 &&
+        _password.contains(RegExp(r'[A-Z]')) &&
+        _password.contains(RegExp(r'[a-z]')) &&
+        _password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
