2ca574376fa6fff0a4b2675d70419a4e05ac0abb
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 11:50:13 +0200

    Fix task 10

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -2,12 +2,15 @@ import '9-palindrome.dart';
 
 String longestPalindrome(String s) {
   if (s.length < 3) return "none";
+  var best = "none";
   for (var i = 0; i < s.length; i++) {
-    for (var j = i + 1; j < s.length; j++) {
-      if (isPalindrome(s.substring(i, j))) {
-        return s.substring(i, j);
+    for (var j = i + 3; j <= s.length; j++) {
+      final sub = s.substring(i, j);
+      if (isPalindrome(sub) &&
+          (best == "none" || sub.length > best.length)) {
+        best = sub;
       }
     }
   }
-  return "none";
+  return best;
 }
