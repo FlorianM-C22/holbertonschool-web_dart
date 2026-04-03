797b4e813195b2811cd748830eb81b77c50825c0
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 11:42:36 +0200

    Task 7

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,13 @@
+int _basketballPoints(Map<String, int> team) {
+  return (team['Free throws'] ?? 0) * 1 +
+      (team['2 pointers'] ?? 0) * 2 +
+      (team['3 pointers'] ?? 0) * 3;
+}
+
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  final teamAPoints = _basketballPoints(teamA);
+  final teamBPoints = _basketballPoints(teamB);
+  if (teamAPoints > teamBPoints) return 1;
+  if (teamAPoints < teamBPoints) return 2;
+  return 0;
+}
