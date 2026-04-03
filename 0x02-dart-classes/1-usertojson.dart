e00e66127f88c06d3991ed1ded1c07e50601978d
FlorianM-C22 <7484@holbertonstudents.com>
2026-04-03 12:08:37 +0200

    Task 1 more fix

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -1,7 +1,9 @@
 class User {
-  String name = "";
-  int age = 0;
-  double height = 0.0;
+  String name;
+  int age;
+  double height;
+
+  User({required this.name, required this.age, required this.height});
 
   Map<String, dynamic> toJson() {
     return {'name': name, 'age': age, 'height': height};
